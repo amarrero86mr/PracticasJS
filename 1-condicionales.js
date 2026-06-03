3ce6551b1e110312c32e003c76c68adc24ea6a8d
amarrero86mr <amarrero86mr@gmail.com>
2026-06-03 16:15:40 -0300

    subiendo practica de condiocionales

--- 1-condicionales.js
+++ 1-condicionales.js
@@ -22,3 +22,48 @@ si no se cumple los requisitos y no hay soborno, se debe mostrar el mensaje "cla
 
 Tarea agregada por la QA enterprise S.A. incorporation
 Testear todas las combinaciones de casos posibles */
+
+// forma de codigo engorrosa, apretujada y poco practica
+
+/* function inspeccionarLocalDeComida(nombreLocal, cumple, donBilletin) {
+    console.log("inspeccionando local: " + nombreLocal)
+    const habilitado = cumple? cumple : donBilletin? donBilletin : false
+    const mensaje = cumple? "todo en regla" : donBilletin? "guiño guiño" : "clausurado!"
+    console.log(mensaje)
+    return habilitado
+}
+console.log(inspeccionarLocalDeComida("Cutre Puaj!", true, false)) */
+
+function inspeccionarLocalDeComida(nombreLocal, cumple, donBilletin) {
+    if (typeof nombreLocal !== "string") { 
+        console.error(cumple + " no es texto")
+        return false
+    }
+    if (typeof cumple !== "boolean") { 
+        console.error(cumple + " no es booleano")
+        return false
+    }
+    if (typeof donBilletin !== "boolean") {
+        console.error(cumple + "no es booleano")
+        return false
+    }
+    
+    console.log("inspeccionando local: " + nombreLocal)
+    
+    let habilitado;
+
+    if (!cumple && !donBilletin) {
+        console.log("clausurado!")
+        habilitado = false
+    } else if (!cumple && donBilletin){
+        console.log("guiño guiño")
+        habilitado = true
+    } else if (cumple) {
+        console.log("todo en regla")
+        habilitado = true
+    }
+
+    return habilitado
+}
+
+console.log(inspeccionarLocalDeComida("Cutre Puaj!",1, false)) 
