ccff9d5587ce879be2bde21876f709204dab26fa
amarrero86mr <amarrero86mr@gmail.com>
2026-06-03 17:17:51 -0300

    preparando ejercicio de DOM

---
+++ dom/DOM.js
@@ -0,0 +1,7 @@
+/* 
+    TAREA:
+crear una vista que contenga 3 elementos
+un elemento donde se pueda visualizar un número ( por defecto 0 )
+un btn con el signo de resta que al hacerle click, reste 1 al número visible
+un btn con el signo de suma que al hacerle click, sume 1 al número visible
+*/
