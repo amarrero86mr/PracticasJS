b2781ea08484bb1cfa65fc3fe9781b726ba4c3a0
amarrero86mr <amarrero86mr@gmail.com>
2026-09-25 01:05:50 -0300

    agregando ejercicio de flatMap

---
+++ elMundoJs/metodosArray/ejercicio.flatMap.js
@@ -0,0 +1,43 @@
+/* 
+.flatMap()
+Ejercicio 5.1: Extractor de hashtags
+
+Método: .flatMap()
+
+Qué debe hacer la función: Separar los hashtags de cada publicación y devolver un único array plano con todos los hashtags individuales.
+
+Argumento que recibe: Un array de strings (frases de hashtags). Ej: ["#js #web", "#css #design"]
+*/
+const hashtags = ["#js #web", "#css #design", "#html #css"];
+function listadoIndividual(lis) {
+    return lis.flatMap(par => par.split(" "));
+};
+console.log(listadoIndividual(hashtags))
+
+function listadoIndividualFor(lis) {
+    const hashindividuales = [];
+    lis.flatMap(par => {
+        let palabra = "";
+        for (let x = 0; x < par.length; x++) {
+            if (par[x] != " ") {
+                palabra = palabra.concat(par[x]);
+            } else if (par[x] === " " || x === par.length-1){
+                hashindividuales.push(palabra);
+                palabra = "";
+            }
+            console.log(palabra)
+        };
+    });
+    return hashindividuales
+};
+console.log(listadoIndividualFor(hashtags))
+
+/* 
+Ejercicio 5.2: Aplanador de habilidades
+
+Método: .flatMap()
+
+Qué debe hacer la función: Devolver un único array plano con todas las habilidades (skills) de todos los empleados sin subarrays anidados.
+
+Argumento que recibe: Un array de objetos con las claves empleado y skills (donde skills es un array de strings). Ej: [{ empleado: "Luis", skills: ["JS", "React"] }, { empleado: "Maria", skills: ["Python", "SQL"] }] 
+*/
--- elMundoJs/metodosArray/flatMap.md
+++ elMundoJs/metodosArray/flatMap.md
@@ -1,15 +1,19 @@
-.flatMap()
-Aplica una transformación a cada elemento (como .map()) y luego aplana el resultado 1 nivel de profundidad.
+## [.flatMap()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap)
 
-Con array simple:
+Aplica una transformación a cada elemento (como `.map()`) y luego aplana el resultado 1 nivel de profundidad. no modifica el array original.
 
-JavaScript
+* Con array simple:
+
+```JavaScript
 const frases = ["hola mundo", "javascript moderno"];
 const palabras = frases.flatMap(f => f.split(" "));
 // ["hola", "mundo", "javascript", "moderno"]
-Con array de objetos:
+```
 
-JavaScript
+* Con array de objetos:
+
+```JavaScript
 const pedidos = [{ cliente: "Ana", tags: ["tech", "oferta"] }, { cliente: "Luis", tags: ["hogar"] }];
 const todosLosTags = pedidos.flatMap(p => p.tags);
 // ["tech", "oferta", "hogar"]
+```
